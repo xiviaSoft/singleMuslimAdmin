6b42b9e59c8aec62e12f2342736508ee52737df5
akram-chopdawg <akram@chopdawg.com>
2025-08-27 19:38:59 +0800

    Implement code changes to enhance functionality and improve performance

---
+++ collections.ts
@@ -0,0 +1,17 @@
+export enum FIREBASE_COLLECTION {
+    USERS = 'users',
+    HELP_SUPPORT = 'helpSupport',
+    TERMS_CONDITIONS = 'termsAndConditions',
+    ADMINS = 'admins',
+    NOTIFICATIONS = 'notifications',
+    FRIENDS = 'friends',
+    CHAT = 'chat',
+    REPORTS = 'reports',
+    REVIEWS = 'reviews',
+    PRODUCTS = 'products',//Future work
+    ORDERS = 'orders',//Future work
+    CATEGORIES = 'categories',//Future work
+    FAQ = 'faq',
+    OFFERS = 'offers',//Future work
+    ACTIVITY_LOGS = 'activityLogs',
+}
